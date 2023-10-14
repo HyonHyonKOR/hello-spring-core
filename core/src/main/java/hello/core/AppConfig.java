5c4845f271060bd04ea10a329310ceee96207d73
HyonHyon <khm3626@gmail.com>
2023-10-14 23:03:44 +0900

    Add files via upload

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,7 @@
 package hello.core;
 
+
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -11,25 +11,30 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class AppConfig {
-
     @Bean
-    public MemberService memberService(){
-        return  new MemberServiceImpl(memberRepository());
+    public MemberService memberService() {
+        //1번
+        System.out.println("call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
     }
     @Bean
-    public MemberRepository memberRepository(){
-        return  new MemoryMemberRepository();
+    public OrderService orderService() {
+        //1번
+        System.out.println("call AppConfig.orderService");
+        return new OrderServiceImpl(
+                memberRepository(),
+                discountPolicy());
     }
     @Bean
-    public OrderService orderService(){
-        return new OrderServiceImpl(memberRepository(),discountPolicy());
+    public MemberRepository memberRepository() {
+        //2번? 3번?
+        System.out.println("call AppConfig.memberRepository");
+        return new MemoryMemberRepository();
     }
     @Bean
-    public DiscountPolicy discountPolicy(){
-       // return  new FixDiscountPolicy();
+    public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 }
